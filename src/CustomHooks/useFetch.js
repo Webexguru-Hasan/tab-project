993b432df7c6f5a9afcceb094fcb805d498dc3ca
Hasan Wazid <Webexguruhasan95@gmail.com>
2023-04-06 11:48:45 -0700

    [completed] tour project successfully

--- src/CustomHooks/useFetch.js
+++ src/CustomHooks/useFetch.js
@@ -1,22 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const useFetch = (url) => {
-    const [data, setData] = useState([])
-    const [loading, setLoading] = useState(false)
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    const removeTour = (id) => {
-         const newTour = data.filter((x) => x.id !== id);
-         setData(newTour)
-    }
+  const removeTour = (id) => {
+    const newTour = data.filter((x) => x.id !== id);
+    setData(newTour);
+  };
 
-    useEffect(() => {
-        fetch(url)
-        .then((res) => res.json())
-        .then((datas) => setData(datas))
-        setLoading(true)
-    }, [url])
-    return {data, loading, removeTour}
-   
-}
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((datas) => setData(datas));
+    setLoading(true);
+  }, [url]);
+  return { data, loading, removeTour };
+};
 
-export default useFetch
+export default useFetch;
