7d1248283cd87d4330149f82f2243948070f9739
Hasan Wazid <Webexguruhasan95@gmail.com>
2023-06-10 22:44:53 -0700

    [COMPLETED] tabs-project all functionality with features

--- src/Components/Tabs.jsx
+++ src/Components/Tabs.jsx
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+const url = "https://course-api.com/react-tabs-project";
 
 const Tabs = () => {
-  return <div>Tabs</div>;
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+  console.log(value);
+  console.log(jobs[value]);
+  console.log(loading);
+
+  //   data fetch start here
+
+  useEffect(() => {
+    async function getDataofTabs() {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setJobs(data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getDataofTabs();
+  }, []);
+
+  const { company, dates, duties, title } = jobs[value];
+
+  return (
+    <>
+      <section className="section">
+        <div className="title">
+          <h2>Experiences</h2>
+          <div className="underline"></div>
+        </div>
+        <div className="jobs-center">
+          {/* btn-container */}
+          <div className="btn-container">
+            {jobs?.map((item, index) => {
+              return (
+                <button
+                  key={item.id}
+                  onClick={() => setValue(index)}
+                  className={`job-btn ${index === value && "active-btn"}`}
+                >
+                  {item.company}
+                </button>
+              );
+            })}
+          </div>
+          {/* job-info */}
+          <article className="job-info">
+            <h3>{title}</h3>
+            <h4>{company}</h4>
+            <p className="job-date">{dates}</p>
+            {duties?.map((duty, index) => {
+              return (
+                <div key={index} className="job-desc">
+                  <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                  <p>{duty}</p>
+                </div>
+              );
+            })}
+          </article>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Tabs;
